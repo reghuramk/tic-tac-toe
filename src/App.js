704d2522c605ee2f1ec56a04d4fd901eb100f4dd
reghuramk <reghuramk04@gmail.com>
2022-09-08 12:52:18 +0530

    Reset button implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+// Material UI
+import Button from '@mui/material/Button';
+
+const App = () =>  {
+
+    const [ reset, setReset ] = useState(false)
+
+  const handleResetClick = () => {
+    setReset(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button onClick={handleResetClick} variant="contained">Reset</Button>
     </div>
   );
 }
